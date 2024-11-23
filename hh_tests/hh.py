1c7c35cf637c5c0bec36fcc69ecc6986ae359512
Your Name <you@example.com>
2024-11-23 12:25:04 +0300

    hh_tests

--- hh_tests/hh.py
+++ hh_tests/hh.py
@@ -18,24 +18,27 @@ class TestBase():
       options = Options()
       options.add_argument('--headless')
       driver=webdriver.Firefox(options=options)
+      driver.maximize_window()
       yield driver
       driver.close()
         
-   
-   @pytest.mark.tags("crirical_path", "login")   TBD
+   @allure.feature('check remote job checklist in vacancies search')
+   @allure.story('vacancies search') 
+   @pytest.mark.tags("crirical_path", "login")   
    def test_checklist_remote_work(self, browser: webdriver.Firefox):
       with allure.step(f"Given: I've go to vacancies search {BASE_URL} url"):
          browser.get(BASE_URL+"/search/vacancy")
       with allure.step("I've found remote job selector"):
-         remote_job_selector=browser.find_element(By.XPATH, "//input[@type='checkbox' and @name='schedule']").find_element(By.XPATH, "./../../..");
+         remote_job_selector=browser.find_element(By.XPATH, "//input[@type='checkbox' and @name='schedule' and value='remote']").find_element(By.XPATH, "./../../..");
       with allure.step("I click on it"):
          remote_job_selector.click()
       with allure.step("I am on a search vacancies page by, the url contains remote jobs query"):
          url = selenium.getLocation()
          assert url.index('schedule=remote')!=-1         
          
-         
-   @pytest.mark.tags("crirical_path", "login")   TBD
+   @allure.feature('check login pathway')      
+   @pytest.mark.tags("crirical_path", "login")   
+   @allure.story('login') 
    @pytest.mark.parametrize("name, password", [(os.getenv("USERNAME"), os.getenv("PASSWORD"))] )
    def test_success_login_by_password(self, browser: webdriver.Firefox , name, password):
       with allure.step("Given I am login page:"):
@@ -49,10 +52,10 @@ class TestBase():
             )
          )
       with allure.step("I enter login data"):
-         name_field = browser.find_element(By.XPATH, "input[data-qa='login-input-password']")
+         name_field = browser.find_element(By.XPATH, "//input[data-qa='login-input-password']")
          print(name)
          name_field.send_keys(name)
          password_field.send_keys(password)
-         browser.find_element(By.XPATH, "button[data-qa='account-login-submit']").click()
+         browser.find_element(By.XPATH, "//button[data-qa='account-login-submit']").click()
       with allure.step("And it cookies contains crypted id"):
          assert browser.get_cookie('crypted_id')
