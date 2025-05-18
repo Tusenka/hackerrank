8143f12a21089a1d5825d4bbe2c11e3cbcd6efc1
tusenka <ourtusenka@yandex.ru>
2025-05-18 07:24:29 +0300

    add dp tasks for dp

--- triangle.py
+++ triangle.py
@@ -1,22 +1,21 @@
-def _for_exists():
-    pass
-
-def _iterate_cut(a:tuple, l, r):
-    for i in range(l, r-1):
-        for j in range(i+1, r):
-            for k in range(r, i, -1):
-                yield a[i]*a[j]*a[k], (l, i), (k, r)
+from functools import lru_cache
 
 
 def _triangle(a):
     _dp=[[ 0 for _ in range(len(a))] for _ in range(len(a))]
-    for k in range(3, len(a)):
+    for k in range(2, len(a)):
         for i in range(len(a)-k):
             _max=0
-            for _cut in _iterate_cut(a, i, i+k):
-                _cost=_cut[0]+_dp[_cut[1][0]][_cut[1][1]]+_dp[_cut[2][0]][_cut[2][1]]
+            for j in range(i+1, i+k):
+                _cost=a[i]*a[i+k]*a[j]+_dp[i+1][j-1]+_dp[j+1][i+k-1]
                 if _cost>_max:
-                   _max=_cost
+                    _max=_cost
+                _cost=_dp[i][j-1]+_dp[j][i+k]
+                if _cost>_max:
+                    _max=_cost
+            _cost=_dp[i][i+k-1]+_dp[i+k][i+k]
+            if _cost>_max:
+                _max=_cost
             _dp[i][i+k]=_max
     return _dp[0][-1]
 
