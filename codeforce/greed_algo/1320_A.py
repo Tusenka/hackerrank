dfd1b30593f19c45ebd23b5aa8f2a82e0c0c8f4b
Tusenka <ourtusenka@yandex.ru>
2026-03-24 09:55:41 +0300

    add codeforce greed algo from train

---
+++ codeforce/greed_algo/1320_A.py
@@ -0,0 +1,18 @@
+# https://codeforces.com/problemset/problem/1320/A
+
+from mypy.checkpattern import defaultdict
+
+
+def solve(a:list[int]):
+    res=defaultdict(int)
+    h=[a[i]-i for i in range(len(a))]
+
+    for i in range(len(h)):
+        res[h[i]]+=a[i]
+
+    return max(res.values())
+
+
+n=int(input())
+a=list(map(int,input().split()))
+print(solve(a))
