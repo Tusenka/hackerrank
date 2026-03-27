74cef55e29fd09ba0a9c8fd79fbf5bfd017c18ed
Tusenka <ourtusenka@yandex.ru>
2026-03-27 09:37:40 +0300

    add codeforce greed algo from train

---
+++ codeforce/greed_algo/1369_C.py
@@ -0,0 +1,29 @@
+# https://codeforces.com/problemset/problem/1369/C
+
+def solve(a: list[int], w: list[int]):
+    a.sort()
+    w.sort()
+    ans=0
+    l=0
+    r=len(a)-1
+    for x in w:
+        if x==1:
+            ans+=2*a[r]
+            r-=1
+            continue
+        ans+=a[l]+a[r]
+        l+=x-1
+        r-=1
+
+    return ans
+
+
+t=int(input())
+
+for _ in range(t):
+    n,k=tuple(map(int,input().split()))
+
+    a=list(map(int,input().split()))
+    w=list(map(int,input().split()))
+
+    print(solve(a,w))
